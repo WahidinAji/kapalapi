75d07cae1df491701f8d9f8257dcfc9e0bdba223
WahidinAji <a17wahidin@gmail.com>
2023-01-07 03:25:28 +0700

    nyerah cors, allow semua aja wkwkwk. masak ga bisa ke custom subdomain dari githubpage. parahhhhhh

--- main.go
+++ main.go
@@ -69,7 +69,7 @@ func main() {
 	app := fiber.New()
 	//set cors
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "https://wahidinaji.github.io/fetch-api-with-github-page/,*.takakiyo.my.id,https://kapalapi-cakrawala.koyeb.app/vessel-keys",
+		AllowOrigins: "*",
 		// AllowOrigins: "https://wahidinaji.github.io/fetch-api-with-github-page/,https://kapalapi.takakiyo.my.id,https://takakiyo.my.id,http://kapalapi.takakiyo.my.id,http://takakiyo.my.id,https://kapalapi-cakrawala.koyeb.app/vessel-keys",
 		AllowHeaders: "Origin, Content-Type, Accept,Bearer",
 		AllowMethods: "GET, POST, PATCH, PUT, DELETE",
